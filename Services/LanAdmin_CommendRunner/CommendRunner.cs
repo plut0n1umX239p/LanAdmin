93317ff50b59f1c2522977c045d8fcb97663109e
plut0n1umx239p <plut0n1um239p@gmail.com>
2022-09-13 16:20:50 +0430

    re create project + add windows feature searcher

---
+++ Services/LanAdmin_CommendRunner/CommendRunner.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+namespace LanAdmin_CommendRunner;
+
+public class CommendRunner
+{
+    public static Process? StartPowerShell()
+    {
+        Process process = new();
+
+        //create a cmd proccess
+        try
+        {
+            process.StartInfo.FileName = "PowerShell.exe";
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+
+            process.StandardInput.WriteLine("echo \"init-ok\"");
+            while (process.StandardOutput.ReadLine() != "init-ok") ;
+
+            return process;
+        }
+        catch
+        {
+            try { process.Close(); } catch { }
+            return null;
+        }
+    }
+    public static string[] RunCommend(Process process, string commend)
+    {
+        process.StandardInput.WriteLine(commend);
+        process.StandardInput.WriteLine("echo \"commend-ok\"");
+
+        List<string> output = new();
+        while (true)
+        {
+            string? input = process.StandardOutput.ReadLine();
+            if(input == null) continue;
+
+            if(input.StartsWith("PS")) continue;
+            if(input == "commend-ok") return output.ToArray();
+
+            output.Add(input);
+        }
+    }
+    public static int CheckForError(string[] commendResualt)
+    {
+        foreach (string i in commendResualt)
+        {
+            if (i.EndsWith("The requested operation requires elevation.")) return -1;// ERR: permision error
+        }
+
+        return 0;// OK: end of checks
+    }
+    public static void StopPowerShell(Process process)
+    {
+        process.Kill();
+    }
+}
