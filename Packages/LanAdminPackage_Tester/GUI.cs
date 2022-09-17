ac0f715e3ad87aa03888d70d7f42338c3e5b25b2
plut0n1umx239p <plut0n1um239p@gmail.com>
2022-09-17 13:23:27 +0430

    add bootstrap support and make it defualt

--- Packages/LanAdminPackage_Tester/GUI.cs
+++ Packages/LanAdminPackage_Tester/GUI.cs
@@ -8,6 +8,22 @@ public class GUI : IGUI_responder
 
     public void ResponderAction(HttpListenerContext _context)
     {
-        GUI_helpers.SendHtmlRespond(_context, "<p>Hello World!</p>");
+        GUI_helpers.SendHtmlRespond(_context, 
+            $"<!DOCTYPE html>" +
+            $"<html lang=\"en-US\">" +
+
+            $"<head>" +
+                $"<meta charset=\"utf-8\">" +
+                $"<title>{Name}/GUI</title>" +
+                $"{GUI_helpers.BootStrapInternal()}" +
+            $"</head>" +
+
+            $"<body>" +
+                $"<div class=\"container p-5 my-5 bg-primary text-white\">" +
+                    $"<h1 class=\"text-center\">Hello World!</h1>" +
+                $"</div>" +
+            $"</body>" +
+
+            $"</html>");
     }
 }
